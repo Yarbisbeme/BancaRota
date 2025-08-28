728a068a741bbabf948d24678d1918590b8f06ca
yarbisbeme <yarbisbem@gmail.com>
2025-08-28 16:40:51 -0400

    Left SideBar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/SideBar/MobileNav";
 import SideBar from "@/components/SideBar/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -11,7 +13,20 @@ export default function RootLayout({
   return (
     <main className="flex min-h-screen w-full font-inter">
       <SideBar user={logedIn.firstName}/>
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image 
+            src={'/icons/logo.svg'} 
+            alt="Menu Icon" 
+            width={30} 
+            height={30} 
+          />
+          <div>
+            <MobileNav user={logedIn} />
+          </div>
+        </div>
       {children}
+      </div>
     </main>
   );
 }
