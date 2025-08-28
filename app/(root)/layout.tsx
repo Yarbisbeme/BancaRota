a156dd4b6e51c18f951d65b2b323f810a124a83b
yarbisbeme <yarbisbem@gmail.com>
2025-08-28 11:01:14 -0400

    Creando primeros componentes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,16 @@
+import SideBar from "@/components/SideBar/SideBar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const logedIn = { firstName: 'Yarbis', lastName: 'Beltré Mercedes' };
+
   return (
-    <main>
-      sidebar
+    <main className="flex min-h-screen w-full font-inter">
+      <SideBar user={logedIn.firstName}/>
       {children}
     </main>
   );
